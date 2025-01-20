99d7cd20ec62278840a896c8f6abad6f59d3b8f5
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-21 01:56:46 +0500

    Update App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { ThemeSwitchComponent } from "./components/theme-switch/theme-switch.component";
+import { ThemeSubmitComponent } from "./components/theme-submit/theme-submit.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ThemeSwitchComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
