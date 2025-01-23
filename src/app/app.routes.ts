17ea1ba36d55df09610debd40a923c6495918121
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-24 01:38:30 +0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,45 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        // component: HomeComponent
-    }
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+     {
+        path: 'About',  // Add this temporary route
+        redirectTo: 'about',
+        pathMatch: 'full'
+    },
+    {
+        path: 'projects',
+        component: ProjectsComponent
+    },
+    {
+        path: 'skills',
+        component: SkillsComponent
+    },
+    {
+        path: 'experience',
+        component: ExperienceComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
 ];
