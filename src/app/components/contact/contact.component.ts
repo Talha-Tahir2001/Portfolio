c3031bed32cc283aeb92c676d7364fbd755b4956
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-25 01:10:37 +0500

    Add Contact Page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
+import { environment } from '../../../environments/environment.development';
+interface ContactForm {
+  name: string;
+  email: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ContactComponent {
-
+  form: ContactForm = {
+    name: '',
+    email: '',
+    message: '',
+  };
+  send() {
+    const templateParams = {
+      from_name: this.form.name,    
+      from_email: this.form.email, 
+      message: this.form.message,  
+    };
+    console.log('Form submitted:', this.form);
+    emailjs
+      .send(
+        environment.ServiceID,
+        environment.TemplateID,
+        templateParams,
+        { publicKey: environment.PublicKey }
+      )
+      .then((res) => console.log(res));
+  }
 }
