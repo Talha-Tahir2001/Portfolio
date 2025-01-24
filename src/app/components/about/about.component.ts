9b2df99ebecafc2d42732f06c31d6715d9ddba11
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-25 00:56:18 +0500

    Update About Component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,7 +6,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styles: ``
+  styles: ``,
+  animations: [
+    trigger('fadeInAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(100px)' }),
+        animate('0.5s 175ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+  ],
 })
 export class AboutComponent {
 
