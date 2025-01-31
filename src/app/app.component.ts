e77438021604d8aa457ffd51a586b295dde5d22a
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-02-01 02:23:06 +0500

    Update with Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { ThemeSwitchComponent } from "./components/theme-switch/theme-switch.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ThemeSwitchComponent],
+  imports: [RouterOutlet, HeaderComponent, ThemeSwitchComponent, FooterComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
