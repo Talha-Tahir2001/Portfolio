528805b584a30b013ccd6e0c9cc99a6c36d818fc
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-05-01 00:50:09 +0500

    refactor: migrate contact form from template driven forms to reactive forms
    - add reactive forms
    - Implement form field validation
    - fix error message colors on light/dark mode

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,40 +1,94 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import emailjs from '@emailjs/browser';
 import { environment } from '../../../environments/environment.development';
-interface ContactForm {
-  name: string;
-  email: string;
-  message: string;
-}
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styles: ``,
 })
 export class ContactComponent {
-  form: ContactForm = {
-    name: '',
-    email: '',
-    message: '',
-  };
-  send() {
+  toast = inject(ToastrService);
+
+  fb = inject(FormBuilder);
+  contactForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
+    ],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', [Validators.required, Validators.minLength(10)]],
+  });
+
+  send(): void {
+    if (this.contactForm.invalid) {
+      this.toast.warning(
+        'Please fill all fields correctly.',
+        'Form Incomplete',
+        {
+          timeOut: 3000,
+          progressBar: true,
+          tapToDismiss: true,
+          progressAnimation: 'increasing',
+          closeButton: true,
+          positionClass: 'toast-bottom-right',
+        }
+      );
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    const { name, email, message } = this.contactForm.value;
     const templateParams = {
-      from_name: this.form.name,    
-      from_email: this.form.email, 
-      message: this.form.message,  
+      from_name: name,
+      from_email: email,
+      message: message,
     };
-    console.log('Form submitted:', this.form);
+
     emailjs
-      .send(
-        environment.ServiceID,
-        environment.TemplateID,
-        templateParams,
-        { publicKey: environment.PublicKey }
-      )
-      .then((res) => console.log(res));
+      .send(environment.ServiceID, environment.TemplateID, templateParams, {
+        publicKey: environment.PublicKey,
+      })
+      .then(() => {
+        this.toast.success('Message sent successfully!', 'Success', {
+          timeOut: 3000,
+          progressBar: true,
+          tapToDismiss: true,
+          progressAnimation: 'increasing',
+          closeButton: true,
+          positionClass: 'toast-bottom-right',
+        });
+        this.contactForm.reset();
+      })
+      .catch((err) => {
+        console.error(err);
+        this.toast.error('Failed to send message. Please try again.', 'Error', {
+          timeOut: 3000,
+          progressBar: true,
+          tapToDismiss: true,
+          progressAnimation: 'increasing',
+          closeButton: true,
+          positionClass: 'toast-bottom-right',
+        });
+      });
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  get message() {
+    return this.contactForm.get('message');
   }
 }
